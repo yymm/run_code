c81c405e4cdb536192d8fcc2ffcfcc3e4e7199d5
yuya yano <yuya.yano.6260@gmail.com>
2013-10-10 19:12:16 +0900

    add a textarea highlighter - codemirror

--- static/js/script.js
+++ static/js/script.js
@@ -5,14 +5,24 @@ function change_theme() {
 		$("body").css({
 			"background" : "url('static/css/img/light-noise.png') repeat 0 0 #fff",
 			"color" : "#555"
-		})
+		});
+		editor.setOption("theme", "solarized dark");
 	}
 	else
 	{
 		$("body").css({
 			"background" : "url('static/css/img/dark-noise.png') repeat 0 0 #fff",
 			"color" : "#ccc"
-		})
+		});
+		editor.setOption("theme", "solarized light");
 	}
 	cnt += 1;
 }
+
+$(document).ready( function() {
+	lang_list = new Array('html', 'python', 'ruby')
+	for (var i = 0; i < lang_list.length; i++)
+	{
+		$('.lang-select').append('<option id=' + lang_list[i] + '>' + lang_list[i] + '</option>')
+	}
+});
