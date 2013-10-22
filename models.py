455e1257170eaed5f0fea53f9a88e01c6b065f34
yuya yano <yuya.yano.6260@gmail.com>
2013-10-22 18:23:25 +0900

    Added models.py

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask.ext.sqlalchemy import SQLAlchemy
+from app import app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/run_code.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
